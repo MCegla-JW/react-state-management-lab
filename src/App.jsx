84efbc78dc14fb271c1f1683acdf040548d7d3bd
MCegla-JW <m.cegla93@gmail.com>
2025-11-13 09:52:04 +0000

    created logic for adding fighter to the team array

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,129 @@
-const App = () => {
+import { useState } from 'react';
+import './App.css';
 
-  return (
-    <h1>Hello world!</h1>
-  );
-}
+const App = () => {
+  const [team, setTeam] = useState([]);
+  const [money, setMoney] = useState(100);
+  const [zombieFighters, setZombieFighters] = useState([
+    {
+      id: 1,
+      name: 'Survivor',
+      price: 12,
+      strength: 6,
+      agility: 4,
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/0c2d6b.png',
+    },
+    {
+      id: 2,
+      name: 'Scavenger',
+      price: 10,
+      strength: 5,
+      agility: 5,
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/033a16.png',
+    },
+    {
+      id: 3,
+      name: 'Shadow',
+      price: 18,
+      strength: 7,
+      agility: 8,
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/262c36.png',
+    },
+    {
+      id: 4,
+      name: 'Tracker',
+      price: 14,
+      strength: 7,
+      agility: 6,
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/3c1e70.png',
+    },
+    {
+      id: 5,
+      name: 'Sharpshooter',
+      price: 20,
+      strength: 6,
+      agility: 8,
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/4b2900.png',
+    },
+    {
+      id: 6,
+      name: 'Medic',
+      price: 15,
+      strength: 5,
+      agility: 7,
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/5a1e02.png',
+    },
+    {
+      id: 7,
+      name: 'Engineer',
+      price: 16,
+      strength: 6,
+      agility: 5,
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/5e103e.png',
+    },
+    {
+      id: 8,
+      name: 'Brawler',
+      price: 11,
+      strength: 8,
+      agility: 3,
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/67060c.png',
+    },
+    {
+      id: 9,
+      name: 'Infiltrator',
+      price: 17,
+      strength: 5,
+      agility: 9,
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/ac3220.png',
+    },
+    {
+      id: 10,
+      name: 'Leader',
+      price: 22,
+      strength: 7,
+      agility: 6,
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/e41f26.png',
+    },
+  ]);
+
+  const handleAddFighter = (fighter) => {
+    // setZombieFighters(fighter); this replaces the entire zombieFighters array with a single fighter object
+    // checking if enough money 
+    if (money >= fighter.price) {
+      setMoney(money - fighter.price)
+      // Adding the fighter 
+        const newTeamArray = [...team, fighter];
+        setTeam(newTeamArray)
+        console.log(team);
+
+      // }
+      } else {
+        console.log('Not enough money');
+    }
+  }
+
+return (
+  <>
+    <h1>Zombie Fighters</h1>
+    <section>
+      <h2>Money: ${money} </h2>
+      <h3>Fighters</h3>
+      <ul>
+        {zombieFighters.map((zombieFighter) => (
+          <li key={zombieFighter.id}>
+            <p>Name: {zombieFighter.name}</p>
+            <p>Price: {zombieFighter.price}</p>
+            <p> Strenght: {zombieFighter.strength}</p>
+            <p>Agility: {zombieFighter.agility}</p>
+            <img src={zombieFighter.img} />
+            <button onClick={() => handleAddFighter(zombieFighter)}>Add Fighter</button>
+          </li>
+        ))};
+      </ul>
+    </section>
+  </>
+);
+};
 
 export default App
